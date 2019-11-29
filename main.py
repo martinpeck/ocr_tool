6d2d2c18aba4a04207d66903513f766ed04b5014
Martin Peck <martinjohnpeck@gmail.com>
2019-11-29 14:05:46 +0000

    apply black formatting and linting fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import (
     TextOperationStatusCodes,
-    ComputerVisionErrorException
 )
 from msrest.authentication import CognitiveServicesCredentials
 from dotenv import load_dotenv
@@ -21,7 +20,7 @@ def cli():
 
 
 @cli.command("file")
-@click.argument('filename', type=click.Path(exists=True))
+@click.argument("filename", type=click.Path(exists=True))
 def perform_ocr_of_local_file(filename):
     client = create_computer_vision_client()
     ocr_result = ocr_file(client, filename)
@@ -31,7 +30,7 @@ def perform_ocr_of_local_file(filename):
 
 
 @cli.command("url")
-@click.argument('url')
+@click.argument("url")
 def perform_ocr_of_remote_file(url):
     client = create_computer_vision_client()
     ocr_result = ocr_url(client, url)
@@ -41,9 +40,9 @@ def perform_ocr_of_remote_file(url):
 
 
 def create_computer_vision_client():
-    '''
+    """
     Creates the ComputerVisionClient instance
-    '''
+    """
 
     if "COMPUTER_VISION_SUBSCRIPTION_KEY" in os.environ:
         subscription_key = os.environ["COMPUTER_VISION_SUBSCRIPTION_KEY"]
@@ -60,16 +59,18 @@ def create_computer_vision_client():
         sys.exit(1)
 
     # create the ComputerVisionClient
-    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key)
-)
+    computervision_client = ComputerVisionClient(
+        endpoint, CognitiveServicesCredentials(subscription_key)
+    )
+
     return computervision_client
 
 
 def ocr_file(client, filepath):
-    '''
+    """
     Calls Azure Computer Vision API to read a file stream
     Returns the result object
-    '''
+    """
 
     with open(filepath, "rb") as image_stream:
         recognize_printed_results = client.batch_read_file_in_stream(
@@ -80,20 +81,20 @@ def ocr_file(client, filepath):
 
 
 def ocr_url(client, image_url):
-    '''
+    """
     Calls Azure Computer Vision API to read the contents of a file via a public url
     Returns the result object
-    '''
+    """
 
-    recognize_printed_results = client.batch_read_file(image_url,  raw=True)
+    recognize_printed_results = client.batch_read_file(image_url, raw=True)
     return recognize_printed_results
 
 
 def extract_text_from_ocr_result(client, ocr_result):
-    '''
+    """
     Takes the result object of a Computer Vision batch read operation
     and extracts the text
-    '''
+    """
 
     # the result object contains a call-back URL
     operation_location_remote = ocr_result.headers["Operation-Location"]
@@ -103,9 +104,7 @@ def extract_text_from_ocr_result(client, ocr_result):
 
     # Poll the call-back URL until we get a status that is not "Running" or "NotStarted"
     while True:
-        get_printed_text_results = client.get_read_operation_result(
-            operation_id
-        )
+        get_printed_text_results = client.get_read_operation_result(operation_id)
         if get_printed_text_results.status not in ["NotStarted", "Running"]:
             break
         time.sleep(0.5)
